fe340a306362a8e0b03427f7fe4bf5df403d9c5a
DiazRock <corolariodiaz@gmail.com>
2023-05-02 15:53:08 -0400

    Fast api server file

--- server/fastapi_server.py
+++ server/fastapi_server.py
@@ -1,8 +1,41 @@
-from fastapi import FastAPI
+"""Fast api server module."""
+
+from dependency_injector.wiring import inject, Provide
+from fastapi import FastAPI, Depends, HTTPException
+from pydantic import BaseModel
+from typing import List
+import json
+
+from .containers import Container
+from .services import OrderProcessorCache
+
 
 app = FastAPI()
 
+class Order(BaseModel):
+    id: int
+    item: str
+    quantity: int    
+    price: float
+    status: str 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+class OrderRequest(BaseModel):
+    criterion: str
+    orders: List[Order]
+
+@app.post("/solution")
+@inject
+async def process_orders(orders_and_criterion: OrderRequest, service: OrderProcessorCache = Depends(Provide[Container.service])):
+    if len(orders_and_criterion.orders) == 0:
+        raise HTTPException(status_code = 400, detail="No orders provided")
+    if any(o.price <= 0 for o in orders_and_criterion.orders):
+        raise HTTPException(status_code = 422, detail="Order price must be positive")
+
+    value = await service.process(orders_and_criterion)
+    return value
+
+
+container = Container()
+container.config.redis_host.from_env("REDIS_HOST", "localhost")
+container.config.redis_password.from_env("REDIS_PASSWORD", "password")
+container.wire(modules=[__name__])
